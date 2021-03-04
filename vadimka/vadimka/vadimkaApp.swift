54b7ce9bcd564047b7b1668242c711277bffd2b7
goten123321 <71818609+goten123321@users.noreply.github.com>
2021-03-04 13:29:24 +0300

    Add files via upload

---
+++ vadimka/vadimka/vadimkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vadimkaApp.swift
+//  vadimka
+//
+//  Created by user on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootContentView()
+        }
+    }
+}
